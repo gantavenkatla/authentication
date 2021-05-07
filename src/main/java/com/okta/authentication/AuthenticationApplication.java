aed067118481be94f1f5c6a0b883a790690eae3c
Venkat Ganta <gantavenkatla@gmail.com>
2021-05-07 11:16:47 -0700

    Update AuthenticationApplication.java

--- src/main/java/com/okta/authentication/AuthenticationApplication.java
+++ src/main/java/com/okta/authentication/AuthenticationApplication.java
@@ -2,12 +2,24 @@ package com.okta.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 
 @SpringBootApplication
 public class AuthenticationApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationApplication.class, args);
 	}
 
+	public class CustomContainer implements
+			WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+
+		@Override
+		public void customize(TomcatServletWebServerFactory factory) {
+			factory.setContextPath("");
+			factory.setPort(8080);
+		}
+	}
 }
